3eafac82b6d976836b938d6f765137e4925b1ba9
Tanyo Ivanov <tivanov@takerman.net>
2024-11-12 21:49:28 +0200

    aa

--- Takerman.Backups.Services/Abstraction/IPackagesService.cs
+++ Takerman.Backups.Services/Abstraction/IPackagesService.cs
@@ -11,9 +11,9 @@ namespace Takerman.Backups.Services.Abstraction
         Task CreateBackupPackages();
 
         void DeletePackage(string package, string package1);
-        
+
         List<ProjectDto> GetAll();
-        
+
         List<PackageEntriesDto> GetProjectPackageEntries();
 
         Task<List<PackageDto>> GetProjectPackages(string project);
